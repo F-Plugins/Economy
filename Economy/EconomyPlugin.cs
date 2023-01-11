f1f5ad1eb2aa6dff1b9426e4878c6fdb7c5f2bc9
Felipe Negri <felinegri5@gmail.com>
2023-01-11 19:38:24 -0300

    add shared project props

--- Economy/EconomyPlugin.cs
+++ Economy/EconomyPlugin.cs
@@ -1,4 +1,6 @@
-﻿using OpenMod.API.Plugins;
+﻿using Cysharp.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using OpenMod.API.Plugins;
 using OpenMod.Unturned.Plugins;
 
 [assembly: PluginMetadata("Feli.Economy", DisplayName = "Economy", Author = "Feli", Website = "docs.fplugins.com")]
@@ -9,5 +11,11 @@ internal class EconomyPlugin : OpenModUnturnedPlugin
 {
     public EconomyPlugin(IServiceProvider serviceProvider) : base(serviceProvider)
     {
+    }
+
+    protected override UniTask OnLoadAsync()
+    {
+        Logger.LogInformation("If you have problems while installing this plugin please refer to: https://docs.fplugins.com/plugins/economy.md or discord.fplugins.com");
+        return base.OnLoadAsync();
     }
 }
